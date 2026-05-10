a265bf4137f3d31604769905f77241e7c1b41f9d
MarekBaleja <balejamarek7@gmail.com>
2026-05-10 05:01:19 +0200

    UI 1.0

---
+++ DSTapTap/Assets/_MyScripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Tu si pripravíme "šuflíky" pre tvoje obrazovky
+    public GameObject dungeonScreen;
+    public GameObject leaderboardScreen;
+    public GameObject upgradeScreen;
+
+    // Táto funkcia sa spustí pri štarte hry
+    void Start()
+    {
+        // Na začiatku chceme vidieť Dungeon, ostatné vypneme
+        ShowDungeon();
+    }
+
+    public void ShowDungeon()
+    {
+        dungeonScreen.SetActive(true);
+        leaderboardScreen.SetActive(false);
+        upgradeScreen.SetActive(false);
+    }
+
+    public void ShowLeaderboard()
+    {
+        dungeonScreen.SetActive(false);
+        leaderboardScreen.SetActive(true);
+        upgradeScreen.SetActive(false);
+    }
+
+    public void ShowUpgrade()
+    {
+        dungeonScreen.SetActive(false);
+        leaderboardScreen.SetActive(false);
+        upgradeScreen.SetActive(true);
+    }
+}
