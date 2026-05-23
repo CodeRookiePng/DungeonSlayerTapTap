f5b0bde426a5fed6201e84bc4f4f69d3c4aac678
CodeRookiePng <jaroslavgabrielmikuska08@gmail.com>
2026-05-23 16:49:39 +0200

    CharacterCreator_0,1

---
+++ DSTapTap/Assets/CharacterSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    // Tu v Unity pretiahneš tie objekty vlasov
+    public GameObject hairBowlCut;
+    public GameObject hairMessy;
+
+    // Funkcia pre ľavé tlačidlo
+    public void SelectBowlCut()
+    {
+        hairBowlCut.SetActive(true);
+        hairMessy.SetActive(false);
+    }
+
+    // Funkcia pre pravé tlačidlo
+    public void SelectMessyHair()
+    {
+        hairBowlCut.SetActive(false);
+        hairMessy.SetActive(true);
+    }
+}
